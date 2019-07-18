b15b2f980032dd3649848af9c1182fda4295cebf
togoboy8 <cedrichillah@gmail.com>
2019-07-18 10:59:29 -0400

    first commit

---
+++ src/main/java/com/example/demo/Week5CBasicBullhornApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Week5CBasicBullhornApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Week5CBasicBullhornApplication.class, args);
+	}
+
+}
